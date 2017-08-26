9ce85d860a64286a1d59ee3dc0f5f512e0825046
阿政 <apezheng@azhengdeMacBook-Pro.local>
2017-08-26 16:04:01 +0800

    push

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+var path = require('path');
 module.exports = {
-  entry: './app.js',
-
+  entry: ['whatwg-fetch', './app.js'],
   output: {
     filename: 'bundle.js',
     publicPath: ''
   },
-
+  resolve: {
+    // add alias for application code directory
+    alias:{
+      '~': path.resolve(__dirname)
+    },
+    extensions: [ '', '.js' ]
+  },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
